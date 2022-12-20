a7dd8bab48f8c32222025a670dbe6093bc8682bf
lost1k <only.lost1k@gmail.com>
2022-12-20 04:19:37 +0300

    add hometasks1

---
+++ hometasks1/ht15.py
@@ -0,0 +1,24 @@
+# 1.5 Напишите программу, которая 
+# 1. принимает на вход координаты двух точек и 
+# 2. находит расстояние между ними в 2D пространстве.
+# in
+# - 3
+# - 6
+# - 2
+# - 1
+
+# out
+# 5.099
+import math
+
+xa = int(input('Введите Xa: '))
+xb = int(input('Введите Xb: '))
+ya = int(input('Введите Ya: '))
+yb = int(input('Введите Yb: '))
+
+def coordinate2D(x1,x2,y1,y2):
+    result = round(math.sqrt((x1-y1)**2 + (x2-y2)**2),3)
+    return result
+
+res = coordinate2D(xa,xb,ya,yb)
+print(f'Расстояние между точками X и Y = {res}')
