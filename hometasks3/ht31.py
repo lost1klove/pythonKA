cc45ac341ddcaac78291d4de3b449736f0bf4ae0
lost1k <only.lost1k@gmail.com>
2023-01-09 20:09:53 +0300

    add lesson3 and ht3 task1

---
+++ hometasks3/ht31.py
@@ -0,0 +1,29 @@
+# Задайте список, состоящий из произвольных чисел,
+# 1. количество задаёт пользователь.
+# 2. Напишите программу, которая
+# 3. найдёт сумму элементов списка, стоящих на нечётных позициях(не индексах).
+#
+# in
+# 5
+# out
+# [10, 2, 3, 8, 9]
+# 22
+from random import sample
+n = int(input('Введите N: '))
+new_list = sample(range(1, (n+1)*2), k=n)
+
+def find_summ(list):
+    summ = 0
+    for i in range(0,(len(list)),2):
+        summ += list[i]
+    return summ
+
+print(new_list)
+# our_list = (find_summ(n)[0])
+result = find_summ(new_list)
+
+# print(our_list)
+print(result)
+
+
+
