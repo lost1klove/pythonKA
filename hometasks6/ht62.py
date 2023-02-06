32cdba31665cdde8d7033428840ad2c4c9fef7bb
lost1k <only.lost1k@gmail.com>
2023-02-07 01:41:32 +0300

    edit ht6-2

--- hometasks6/ht62.py
+++ hometasks6/ht62.py
@@ -2,7 +2,7 @@
 #    кратные 20 или 21. Use comprehension.
 
 def fractional_list(n):
-    return [(i,(f'{i} кратно 20')) for i in range(20, n + 1) if i % 20 == 0 or i % 21 == 0]
+    return [i for i in range(20, n + 1) if i % 20 == 0 or i % 21 == 0]
     # TODO допилить вывод
     # [i for i in range(20, n + 1) if i % 20 == 0 and print([f'{i} кратно 20']) or i % 21 == 0 and print([f'{i} кратно 21'])]
 
